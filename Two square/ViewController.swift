8ac540e7e73a3b139c5efa72e0cc4c9697941468
Anna Goncharova <bkljn4g@gmail.com>
2020-08-24 20:57:46 +0300

    first commit

---
+++ Two square/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two square
+//
+//  Created by Anka on 23.08.2020.
+//  Copyright © 2020 Anna Goncharova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
